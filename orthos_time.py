0c117c86a445540c974138d9daa3f6338db7fdf1
Grzegorz Krajewski <gk.krajewski@gmail.com>
2026-04-11 18:08:27 +0200

    Add Orthos Time Library with obfuscated code
    Initial commit of the Orthos Time Library with obfuscated core.

---
+++ orthos_time.py
@@ -0,0 +1,29 @@
+"""
+Orthos Time Library - System 3-9
+Copyright (c) 2026. All Rights Reserved.
+Obfuscated Core.
+"""
+import base64
+
+_p = (
+    "wpPCl8KawpnCnMKeSsKXwovCnsKSNMKTwpfCmsKZwpzCnkrCnsKTwpfCjzQ0wo7Cj8KQSsKR"
+    "wo/CnsKJXcKJY8KJwp7Ck8KXwo9SU2Q0SkpKSsKeSmdKwp7Ck8KXwo9YwpbCmcKNwovClsKe"
+    "wpPCl8KPUlM0SkpKSsKSVkrCl1ZKwp1KZ0rCnljCnsKXwonCksKZwp/CnFZKwp5Ywp7Cl8KJ"
+    "wpfCk8KYVkrCnljCnsKXwonCncKPwo00SkpKSsKdwprCksKPwpzCj0pnSkzChVfCh0xKwpPC"
+    "kEpdSmZnSsKSSmZKY0rCj8KWwp3Cj0pMwoVVwodMNEpKSkrCjcKjwo3ClsKPSmdKwpJKT0pb"
+    "XDRKSkpKwovCmMKRwpbCj8KJwpJKZ0pSwpJKT0pbXEpVSsKXSllKYFpKVUrCnUpZSl1gWlpT"
+    "SlRKXVo0SkpKSsKLwpjCkcKWwo/CicKXSmdKUsKXSlVKwp1KWUpgWlNKVEpgNEpKSkrCi8KY"
+    "wpHClsKPwonCl8KJwpfCmcKOUmdKW0pWSsKLwpjCkcKWwo/CicKXSk9KW2JaNEpKSkrCi8KY"
+    "wpHClsKPwonCksKJwpfCmcKOSmdKwovCmMKRwpbCj8KJwpJKT0pbYlo0SkpKSsKOwpPCkMKQ"
+    "wonCi8KYwpHClsKPSmdKwovCjMKdUsKLwpjCkcKWwo/CicKSwonCl8KmcKOUzRKSkpKwpPC"
+    "kErCjsKTwpDCkMKJwovCmMKRwpbCj0poSmNaZDRKSkpKSkpKSsKOwpPCkMKQwonCi8KYwpHC"
+    "lsKPSmdKW2JaSldKwo7Ck8KQwpDCicKLwpjCkcKWwo80SkpKSsKZwpzCnsKSwpnCnUpnSsKL"
+    "wozCnVJjWkpXSsKOwpPCkMKQwonCi8KYwpHClsKPUzRKSkpKwpzCj8Kewp/CnMKYSsKdwpr"
+    "CksKPwpzCj1ZKwo3Co8KNwpbCj1ZKwpzCmcKfwpjCjlLCmcKcwp7CksKZwp1WSlxTNDTCjsKP"
+    "wpBKwpDCmcKcwpfCi8KewonCnsKTwpfCj1LCncKawpLCj8Kcwo9WSsKNwqPCjcKWwo9WSsKZ"
+    "wpzCnsKSwpnCnVNkNEpKSkrCnMKPwp7Cn8KcwphKwpBMwqXCncKawpLCj8Kcwo/Cp0ptwqPC"
+    "jcKWwo9kSsKlwo3Co8KNwpbCj8KnSsKmSnnCnMKewpLCmcKdZErCpcKZwpzCnsKSwpnCnWRY"
+    "XMKQwqdMNA=="
+)
+
+exec("".join(chr((ord(c) - 42) % 1114112) for c in base64.b64decode(_p).decode('utf-8')))
